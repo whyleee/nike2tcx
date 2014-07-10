18ae901d35bbd963d8cfceeaa1e7bf13a022fa3e
Pavel Nezhencev <whyleee@gmail.com>
2014-07-11 00:57:45 +0300

    Added assembly info

--- nike2tcx/Properties/AssemblyInfo.cs
+++ nike2tcx/Properties/AssemblyInfo.cs
@@ -1,14 +1,11 @@
 ﻿using System.Reflection;
 
 // General info about the product
-[assembly: AssemblyTitle("EndoSync")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EndoSync")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("nike2tcx")]
+[assembly: AssemblyDescription("Simple Nike+ to TCX convertor tool")]
+[assembly: AssemblyCompany("Pavel Nezhencev")]
+[assembly: AssemblyProduct("nike2tcx")]
+[assembly: AssemblyCopyright("Copyright © Pavel Nezhencev 2014")]
 
 // Product version
 [assembly: AssemblyVersion("1.0")]
